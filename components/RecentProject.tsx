d33439321d5e504bea37ffe72e6d0d71e56c1b4a
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2024-10-27 21:51:41 +0300

    day-2-2

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -1,6 +1,7 @@
 
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 export default function RecentProject() {
   return (
@@ -10,8 +11,10 @@ export default function RecentProject() {
       </h1>
     <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10 '>
     {projects.map(({id,title,des,img,iconLists,link})=>(
-      <div key={title}>
-        {title}
+      <div key={title} className='lg:min-h-[32.2rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+        <PinContainer >
+          {title}
+        </PinContainer>
       </div>
     ) )}
     </div>
