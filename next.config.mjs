d0863a107f36cd672e0eea73e2314acb3984fd4c
Mahmoud Majed AL Sheikh <122573361+mahmoudmajedalsheikh@users.noreply.github.com>
2024-11-18 06:03:42 +0200

    the final-5

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
+  output: "export",
   typescript: {
   // !! WARN !!
   // Dangerously allow production builds to successfully complete even if
